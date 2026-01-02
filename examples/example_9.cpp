b8d330f2622efe363ada2c6e66c47f9b92350445
1024ent <shonenent1024@gmail.com>
2026-01-03 04:22:01 +0800

    Updated all comment

--- examples/example_9.cpp
+++ examples/example_9.cpp
@@ -1,8 +1,45 @@
 /**
  * @file example_9.cpp
- * @brief 
+ * @brief Bluetooth Controlled LED Example
  * @copyright UMPSA ROBOTICS
  * @author LOO HUI KIE
  * @date 03/01/2024
  **/
+#include <Arduino.h>
+#include "BluetoothSerial.h"
 
+BluetoothSerial SerialBT;
+
+constexpr int LED_PIN = 13;
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
+
+  Serial.begin(115200);
+  SerialBT.begin("ESP32_LED");  // Bluetooth device name
+  Serial.println("Bluetooth device started, ready to pair!");
+}
+
+void loop() {
+  if (SerialBT.available()) {
+    char cmd = SerialBT.read();
+    
+    // Turn LED on/off based on command
+    if (cmd == '1') {
+      digitalWrite(LED_PIN, HIGH);
+      SerialBT.println("LED ON");
+    } else if (cmd == '0') {
+      digitalWrite(LED_PIN, LOW);
+      SerialBT.println("LED OFF");
+    } else if(cmd == 'T') {
+      digitalWrite(LED_PIN, HIGH);
+      delay(1000);
+      digitalWrite(LED_PIN, LOW);
+      delay(1000);
+      SerialBT.println("LED Blinking");
+    } else {
+      SerialBT.println("Send 1 to turn ON, 0 to turn OFF");
+    }
+  }
+}
